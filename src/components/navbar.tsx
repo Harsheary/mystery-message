593ce515dfb56798bbb72f703b44ead27a59cb01
Harsheary <harsheary2022005@gmail.com>
2025-05-17 10:45:21 -0400

    Minor styling fixes in Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <a href="#" className="text-xl font-bold mb-4 md:mb-0">
-          True Feedback
+          Mystery Message
         </a>
         {session ? (
           <>
@@ -27,7 +27,7 @@ const Navbar = () => {
           </>
         ) : (
           <Link href="/sign-in">
-            <Button className="w-full md:w-auto bg-slate-100 text-black " >Login</Button>
+            <Button className="w-full md:w-auto bg-slate-100 text-black hover:bg-slate-400" >Login</Button>
           </Link>
         )}
       </div>
